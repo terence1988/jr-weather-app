f8335b00ed44d990035d7ae699e13e9bda0aea32
Terence Ma <awstest135@gmail.com>
2020-12-07 10:21:44 +1100

    Keep learning

---
+++ src/components/AddToDo.js
@@ -0,0 +1,14 @@
+class TodoList extends Component {
+  render() {
+    return (
+      <div className="todoListMain">
+        <div className="header">
+          <form onSubmit={this.addItem}>
+            <input placeholder="enter task">
+            </input>
+            <button type="submit">add</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
